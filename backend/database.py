0e96f9ee9384910a6c2956afe48ef45d409b415a
EC2 Default User <ec2-user@ip-172-31-45-49.eu-north-1.compute.internal>
2026-06-16 06:10:28 +0000

    adding systemd and autostart and loggings

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mysql+pymysql://stacklyuser:Stackly%40123@localhost/companydb"
+# Load environment variables from .env
+load_dotenv()
 
+# Read DATABASE_URL from environment
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
 
+# Create session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
+# Base class for models
 Base = declarative_base()
